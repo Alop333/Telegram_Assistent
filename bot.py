21eac114b13c9cbecea0104d868f0c77aca1c449
Alop333 <arturlopes333@gmail.com>
2025-05-10 19:54:11 -0300

    download and show files

--- bot.py
+++ bot.py
@@ -7,6 +7,8 @@ BOT_TOKEN = os.getenv('BOT_TOKEN')
 SENHA_CORRETA = os.getenv('SENHA_CORRETA')
 ARQUIVO_APROVADOS = os.getenv('ARQUIVO_APROVADOS')
 bot = telebot.TeleBot(BOT_TOKEN)
+PASTA_PDFS = "pdfs"
+os.makedirs(PASTA_PDFS, exist_ok=True)
 
 def carregar_ids_aprovados():
     try:
@@ -21,26 +23,62 @@ def salvar_id_aprovado(chat_id):
 
 ids_aprovados = carregar_ids_aprovados()
 
+@bot.message_handler(content_types=['document'])
+def receber_documento(message):
+    chat_id = format(message.chat.id)
+
+    if chat_id in ids_aprovados:
+        documento = message.document
+        file_name = documento.file_name
+
+        if file_name.lower().endswith('.pdf'):
+            bot.reply_to(message, f"Iniciando download de '{file_name}'. Isso pode demorar alguns segundos")
+            file_info = bot.get_file(documento.file_id)
+            file_path = file_info.file_path
+
+            downloaded_file = bot.download_file(file_path)
+            caminho_local = os.path.join(PASTA_PDFS, file_name)
+            with open(caminho_local, 'wb') as f:
+                f.write(downloaded_file)
+
+            bot.reply_to(message, f"PDF '{file_name}' salvo com sucesso!")
+    else:
+        bot.reply_to(message, "Envie a senha para ganhar acesso ao bot")
+
+
 def conversar(message):
-    bot.reply_to(message, message.text)
+    bot.reply_to(message, "bobo")
+
+@bot.message_handler(commands=['files','show'])
+def show_dir(message):
+    nomes_arquivos = [f for f in os.listdir(PASTA_PDFS) if os.path.isfile(os.path.join(PASTA_PDFS, f))]
+    if nomes_arquivos:
+        string_resultado = '\n\n'.join(nomes_arquivos)
+        bot.reply_to(message, f"Aqui está a lista de arquivos presentes na biblioteca do bot:\n{string_resultado}")
+    else:
+        bot.reply_to(message, "A biblioteca do bot está vazia ;-;")
+    
+
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    bot.reply_to(message, "Olá, por favor envie a senha para liberar acesso ao bot")
+    bot.reply_to(message, "Olá, sou um bot para armazenar seus arquivos.")
+    chat_id = format(message.chat.id)
+
+    if chat_id not in ids_aprovados:
+        bot.reply_to(message, "Por favor, envie a senha para ganhar acesso ao bot")
+
 
 @bot.message_handler(func=lambda message: True) 
 def verificar_senha(message):
     chat_id = format(message.chat.id)
-
-    print ("aprovados:", ids_aprovados)
-    print (chat_id)
 
     if chat_id in ids_aprovados:
         conversar(message)
     else:
         if message.text.strip() == SENHA_CORRETA:
             salvar_id_aprovado(chat_id)
-            carregar_ids_aprovados()
+            ids_aprovados.add(chat_id)
             bot.reply_to(message, "Senha correta! Acesso liberado.")
         else:
             bot.reply_to(message, "Você ainda não tem acesso. Por favor, envie a senha")
